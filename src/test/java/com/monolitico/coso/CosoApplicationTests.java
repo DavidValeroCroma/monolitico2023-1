76d5c54acf1ed535794c90510e6acae6e26eb4ef
Cr0m4 <david.valero@usach.cl>
2023-05-12 22:06:36 -0400

    spring initializr

---
+++ src/test/java/com/monolitico/coso/CosoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monolitico.coso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
